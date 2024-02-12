1602ba437778117bc91688f5dc2fbfdf2366884d
Kempeter <70294616+Kempeter@users.noreply.github.com>
2024-02-12 21:04:47 +0100

    Create main.rs

---
+++ GetUserNameA/src/main.rs
@@ -0,0 +1,19 @@
+use std::borrow::Cow;
+use std::fmt::format;
+
+use windows::core::PSTR;
+use windows::Win32::System::WindowsProgramming::GetUserNameA;
+
+fn main() {
+    unsafe {
+        let mut size = 0;
+        let result = GetUserNameA(PSTR::null(), &mut size);
+        let mut lpbuffer = Vec::with_capacity(size as usize);
+        let result = GetUserNameA(PSTR(lpbuffer.as_mut_ptr()), &mut size);
+
+        lpbuffer.set_len(size as usize);
+
+        let user_name = String::from_utf8_lossy(&lpbuffer).to_string();
+        println!("{}", user_name);
+    };
+}
