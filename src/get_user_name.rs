ad829e3c320b85a9e0493bfef0cadc599d8c40a2
Kempeter <70294616+Kempeter@users.noreply.github.com>
2024-02-12 21:11:36 +0100

    Create get_user_name.rs

---
+++ src/get_user_name.rs
@@ -0,0 +1,16 @@
+use windows::core::PSTR;
+use windows::Win32::System::WindowsProgramming::GetUserNameA;
+
+pub fn get_user_name() {
+    unsafe {
+        let mut size = 0;
+        let result = GetUserNameA(PSTR::null(), &mut size);
+        let mut lpbuffer = Vec::with_capacity(size as usize);
+        let result = GetUserNameA(PSTR(lpbuffer.as_mut_ptr()), &mut size);
+
+        lpbuffer.set_len(size as usize);
+
+        let user_name = String::from_utf8_lossy(&lpbuffer).to_string();
+        println!("{}", user_name);
+    };
+}
