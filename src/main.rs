2c7a9712e7fbbfc781722a19f9fe8b112b0bee37
Kempeter <70294616+Kempeter@users.noreply.github.com>
2024-02-18 16:56:08 +0100

    Add get_disk_free_space_a to main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 mod get_user_name;
 mod cursor;
+mod get_disk_free_space_a;
 
 use get_user_name::get_user_name;
 use cursor::cursor;
+use get_disk_free_space_a::get_disk_free_space_a;
 
 fn main() {
-    get_user_name();
+    //get_user_name();
     //cursor();
+    get_disk_free_space_a()
 }
