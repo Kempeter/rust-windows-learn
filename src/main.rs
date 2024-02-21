f2212dacc97a1d299bf49ac9c66256fa7a1b0615
Kempeter <70294616+Kempeter@users.noreply.github.com>
2024-02-21 16:36:55 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
 mod get_user_name;
 mod cursor;
 mod get_disk_free_space_a;
+mod file_manager::file_manager_main;
 
 use get_user_name::get_user_name;
 use cursor::cursor;
 use get_disk_free_space_a::get_disk_free_space_a;
+use file_manager::file_manager_main::file_manager;
 
 fn main() {
     //get_user_name();
     //cursor();
-    get_disk_free_space_a()
+    //get_disk_free_space_a()
+    file_manager();
 }
